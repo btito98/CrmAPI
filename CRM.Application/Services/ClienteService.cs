760bf29876f3c38713cdec54202d429b6aabac63
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2025-03-15 19:53:51 -0300

    refactor: refatora o clienteService

--- CRM.Application/Services/ClienteService.cs
+++ CRM.Application/Services/ClienteService.cs
@@ -9,22 +9,15 @@ namespace CRM.Application.Services
 {
     public class ClienteService : BaseService<ClienteCreateDTO, Cliente, ClienteResultDTO>, IClienteService
     {
-        protected readonly IMapper _mapper;
-        protected readonly IClienteRepository clienteRepository;
-        public ClienteService(IBaseRepository<Cliente> repository, IClienteRepository clienteRepository, IMapper mapper) : base(repository, mapper)
+        protected readonly IClienteRepository _repository;
+        public ClienteService(IClienteRepository repository, IMapper mapper) : base(repository, mapper)
         {
-            this.clienteRepository = clienteRepository;
-            _mapper = mapper;
-        }
-
-        public override async Task<ClienteResultDTO> AddAsync(ClienteCreateDTO dto)
-        {
-            return await base.AddAsync(dto);
+            _repository = repository;
         }
 
         public async Task<(IEnumerable<ClienteResultDTO> clientes, int totalCount)> GetFilteredAsync(ClienteFilterParams filterParams)
         {
-            var (entities, totalCount) = await clienteRepository.GetFilteredAsync(filterParams);
+            var (entities, totalCount) = await _repository.GetFilteredAsync(filterParams);
 
             var clientes = _mapper.Map<IEnumerable<ClienteResultDTO>>(entities);
 
