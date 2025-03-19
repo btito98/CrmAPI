0aaf6f0a311b46b80e7406c24dc0ea9fdff01474
Gabriel <gbbatistasouza@gmail.com>
2025-03-19 12:19:38 -0300

    feat: modifica interfaces para utilizar o result como padrão de retorno

--- CRM.Application/Interfaces/IClienteService.cs
+++ CRM.Application/Interfaces/IClienteService.cs
@@ -1,11 +1,12 @@
 ﻿using CRM.Application.DTOs.Cliente;
 using CRM.Domain.Entities;
 using CRM.Domain.Models.Cliente;
+using CRM.Shared.Results;
 
 namespace CRM.Application.Interfaces
 {
     public interface IClienteService : IBaseService<ClienteCreateDTO, Cliente, ClienteResultDTO>
     {
-        Task<(IEnumerable<ClienteResultDTO> clientes, int totalCount)> GetFilteredAsync(ClienteFilterParams filterParams);
+        Task<Result<(IEnumerable<ClienteResultDTO> clientes, int totalCount)>> GetFilteredAsync(ClienteFilterParams filterParams);
     }
 }
